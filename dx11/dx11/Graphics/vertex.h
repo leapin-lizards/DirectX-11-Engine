1f562551bf53a65ce98547073f2c18c9a77f2acc
leapin-lizards <wonjaechoi2@gmail.com>
2023-08-30 06:55:35 +0900

    added project file

--- dx11/dx11/Graphics/vertex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <DirectxMath.h>
-struct Vertex {
-	Vertex() {}
-	Vertex(float x, float y,float z ,float u,float v) :pos(x, y,z), texCoord(u,v) {}
-	DirectX::XMFLOAT3 pos;
-	DirectX::XMFLOAT2 texCoord;
-};
--- dx11/dx11/stringHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-class StringHelper {
-public:
-	static std::wstring StringToWide(std::string str);
-	static std::string GetDirectoryFromPath(const std::string & path);
-	static std::string GetFileExtension(const std::string & path);
-};
